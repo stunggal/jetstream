6022bae496cb46180712ce2ecdc540787656003e
stunggal <54833890+stunggal@users.noreply.github.com>
2022-06-16 18:54:04 +0700

    15-6-2022

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('/d', DashController::class)->middleware('guest');
+// make route with auth
